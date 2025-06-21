2a17ef82189e6c47f6a945596f03d3ded3b3570c
Haidermirza7072 <haidermirza7071@gmail.com>
2025-06-22 01:11:05 +0530

    Todo list

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-function TodoItem({ todo, deleteTodo, toggleTodo }) {
+function TodoItem({ todo, deleteTodo, toggleTodo, index }) {
   return (
     <div className={`todo-item ${todo.completed ? "completed" : ""}`}>
-      <span onClick={() => toggleTodo(todo.id)}>{todo.text}</span>
+      <span onClick={() => toggleTodo(todo.id)}>
+        {index + 1}. {todo.text}
+      </span>
       <button onClick={() => deleteTodo(todo.id)}>❌</button>
     </div>
   );
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,18 +2,20 @@ import React from "react";
 import TodoItem from "./TodoItem";
 
 function TodoList({ todos, deleteTodo, toggleTodo }) {
-  return (
-    <div>
-      {todos.map((todo) => (
-        <TodoItem
-          key={todo.id}
-          todo={todo}
-          deleteTodo={deleteTodo}
-          toggleTodo={toggleTodo}
-        />
-      ))}
-    </div>
-  );
+    return (
+        <div>
+            {todos.map((todo, index) => (
+                <TodoItem
+                    key={todo.id}
+                    index={index}
+                    todo={todo}
+                    deleteTodo={deleteTodo}
+                    toggleTodo={toggleTodo}
+                />
+            ))}
+
+        </div>
+    );
 }
 
 export default TodoList;
